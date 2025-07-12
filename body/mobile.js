07fc3f678ddb1d231e4a890058af53c8f851540a
Enzo Oliveira <uxen.solutions@gmail.com>
2025-07-11 23:18:07 -0300

    Responsividade finalizada

--- body/mobile.js
+++ body/mobile.js
@@ -1,8 +1,15 @@
-let btnMenuMob = document.querySelector('#btn-menu-mobile')
-let lineOne = document.querySelector('.line-menumob-1')
-let lineTwo = document.querySelector('.line-menumob-2')
+let btnMenuMob = document.querySelector('#btn-menu-mobile');
+let lineOne = document.querySelector('.line-menumob-1');
+let lineTwo = document.querySelector('.line-menumob-2');
+let menuMobile = document.querySelector('#menu-mobile');
 
-btnMenuMob.addEventListener("click", ()=>{
-    lineOne.classList.toogle('active1')
-    lineTwo.classList.toogle('active2')
-})
+
+let body = document.querySelector('body');
+
+btnMenuMob.addEventListener('click', () => {
+    lineOne.classList.toggle('line-menumob-1-active');
+    lineTwo.classList.toggle('line-menumob-2-active');
+    menuMobile.classList.toggle('menu-mobile-active');
+    
+    body.classList.toggle('body-active');
+});
